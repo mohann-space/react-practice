e363041f3ff69d0cea5eb057bc567bb19c510e3d
mohan adithya reddy <mohann1205@gmail.com>
2026-08-18 22:36:02 +0530

    learn: practice useState and event handling

---
+++ src/07-useState-events/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App(){
+    const[count, setCount] = useState(0)
+
+    function increase(){
+        setCount(count + 1);
+    }
+
+    function decrease(){
+        setCount(count - 1);
+    }
+
+    function reset(){
+        setCount(0);
+    }
+
+    return(
+        <div>
+            <h1>Counter</h1>
+
+            <h2>{count}</h2>
+
+            <button onClick={increase}>Increase</button>
+            <button onClick={decrease}>Decrease</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+}
+
+export default App;
