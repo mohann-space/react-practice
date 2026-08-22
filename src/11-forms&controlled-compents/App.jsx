2a9d424eea96c2b49dcfffdaa3f45bc6eac8fe1d
mohan adithya reddy <mohann1205@gmail.com>
2026-08-23 00:17:53 +0530

    feat: practice controlled forms and form submission

--- src/11-forms&controlled-compents/App.jsx
+++ src/11-forms&controlled-compents/App.jsx
@@ -2,14 +2,43 @@ import { useState } from "react";
 
 function App() {
     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Hello ${name}`);
+    };
 
     return (
         <div>
-            <input 
-               type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
+            <h1>React form</h1>
+
+            <form onSubmit={handleSubmit}>
+              <input 
+                 type="text"
+                 value={name}
+                 placeholder="Enter your name"
+                 onChange={(e) => setName(e.target.value)}
+              />
+
+              <br />
+
+              <input 
+                 type="email"
+                 value={email}
+                 placeholder="Enter your email"
+                 onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <br /><br />
+
+              <button type="submit">Submit</button>
+            </form>
+
+            <br />
+
+            <p>Your name: {name}</p>
+            <p>Your email: {email}</p>
         </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './11-forms&controlled-compents/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
