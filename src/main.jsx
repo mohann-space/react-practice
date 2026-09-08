6ae32f6348789a4127835e661883a524ff427f63
mohan adithya reddy <mohann1205@gmail.com>
2026-09-08 23:16:59 +0530

    feat: learn React Router for navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./20-customHook/App.jsx";
+import App from "./21-react-router/App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
